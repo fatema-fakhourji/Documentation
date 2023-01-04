47130cac8dc982d725343f2d19ef2730a589a757
fatema <fatemaalbatoulfakhourji@gmail.com>
2023-01-04 14:54:52 +0200

    step 5

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -5,6 +5,13 @@ let hours = date_ob.getHours();
 let minutes = date_ob.getMinutes();
 let seconds = date_ob.getSeconds();
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+let Mov = movies.join("\n");
 
 app.get("/", (request, response) => {
     response.send("Ok");
@@ -26,6 +33,18 @@ app.get("/search?s=", (req, res) => {
    });
 app.get("/search?s=:SEARCH", (req, res) => {
     res.json({status:200, message:"ok", data:req.params.SEARCH})
+   });
+app.get("/movies/create", (req, res) => {
+    res.json({status:200, message:"ok create"})
+   });
+app.get("/movies/read", (req, res) => {
+    res.json({status:200, data:movies})
+   });
+app.get("/movies/update", (req, res) => {
+    res.json({status:200, message:"ok update"})
+   });
+app.get("/movies/delete", (req, res) => {
+    res.json({status:200, message:"ok delete"})
    });
 app.listen(3000, () => {
     console.log("Listen on the port 3000...");
